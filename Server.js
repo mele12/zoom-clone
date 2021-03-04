87a3b7863899055e4389fd3ab3a0f7964bf4f981
melaku-k <kassamelaku2129@outlook.com>
2021-03-03 18:20:08 -0600

    zoom

--- Server.js
+++ Server.js
@@ -1,25 +1,37 @@
 const express= require ('expess');
 const app = express();
+
 const server = require('http').server(app);
+const io = require('socket.io')(server)
+const { v4: uuidv4 } = require('uuid');
+
+
+app.set('view engine' , 'ejs');
+app.use(express.statics('public'));
 
 app.get('/', (req, res) => {
-    res.send('hi')
-  })
+    res.redirect(`/${uuidv4()}`);
+  });
+
+  app.get('/', (req, res) => {
+    res.render('room', {roomId: req.params.room});
+  });
+
+  io.on('connection',( socket ) => {
+    socket.on('join-room', () => {
+
+      console.log('bini joined');
+    });
+      // socket.join(roomId)
+      // socket.to(roomId).broadcast.emit('user-connected', userId);
+      // // messages
+      // socket.on('message', (message) => {
+      //   //send message to the same room
+      //   io.to(roomId).emit('createMessage', message)
+    }); 
+  
 
 server.listen(process.env.PORT || 3000);
 
-
-
-// const io = require('socket.io')(server)
-// const { ExpressPeerServer } = require('peer');
-// const peerServer = ExpressPeerServer(server, {
-//  debug: true
-// });
-// const { v4: uuidV4 } = require('uuid')
-
-// app.use('/peerjs', peerServer);
-
-// app.set('view engine', 'ejs')
-// app.use(express.static('public'))
 
 
