7fa96c25b1031b7305e604be74edb5bf14bedca3
melaku-k <kassamelaku2129@outlook.com>
2021-03-11 05:47:26 -0600

    go

--- Server.js
+++ Server.js
@@ -1,13 +1,20 @@
-const express= require ('expess');
+const express= require ('express');
 const app = express();
 
-const server = require('http').server(app);
+const server = require('http').Server(app);
 const io = require('socket.io')(server)
+
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+  debug: true
+});
+
 const { v4: uuidv4 } = require('uuid');
 
-
+app.use('/peerjs', peerServer);
 app.set('view engine' , 'ejs');
-app.use(express.statics('public'));
+app.use(express.static('public'));
+
 
 app.get('/', (req, res) => {
     res.redirect(`/${uuidv4()}`);
@@ -17,18 +24,21 @@ app.get('/', (req, res) => {
     res.render('room', {roomId: req.params.room});
   });
 
-  io.on('connection',( socket ) => {
-    socket.on('join-room', (roomId) => {
-
-      console.log('bini joined', roomId);
-    });
-      // socket.join(roomId)
-      // socket.to(roomId).broadcast.emit('user-connected', userId);
-      // // messages
-      // socket.on('message', (message) => {
-      //   //send message to the same room
-      //   io.to(roomId).emit('createMessage', message)
+  io.on('connection', socket => {
+    socket.on('join-room', (roomId, userId) => {
+      socket.join(roomId)
+      socket.to(roomId).broadcast.emit('user-connected', userId);
+      // messages
+      socket.on('message', (message) => {
+        //send message to the same room
+        io.to(roomId).emit('createMessage', message)
     }); 
+  
+      socket.on('disconnect', () => {
+        socket.to(roomId).broadcast.emit('user-disconnected', userId)
+      })
+    })
+  })
   
 
 server.listen(process.env.PORT || 3000);
